78ada5ca7cd92b2c030e2974313ab193408f73ec
Efrain Perez Jr <efrainperezjr@live.com>
2018-04-12 20:57:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
 const currentUser = 'Efrain';
 let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
-let excitedwelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}!`;
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}!`;
 let shortGreeting = `Welcome, ${currentUser}!`;
